162ee56391f45edaecef1a2c5fb92abcc562159c
Rick <rickb@rickb.com>
2021-07-04 15:13:26 -0500

    async

--- Sources/PonyKit/PonyService+Characters.swift
+++ Sources/PonyKit/PonyService+Characters.swift
@@ -24,3 +24,30 @@ public extension PonyService {
 		client.request(PonyAPI.GetCharactersByResidence(residence: residence, parameters: query ?? .init()))
 	}
 }
+
+
+#if swift(>=5.5)
+@available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+public extension PonyService {
+
+	func allCharacters(query: CharacterQueryParameters? = nil) async throws -> Ponies {
+		try await client.request(PonyAPI.GetAllCharacters(parameters: query ?? .init()))
+	}
+
+	func characters(byId id: Int, query: CharacterQueryParameters? = nil) async throws -> Ponies {
+		try await client.request(PonyAPI.GetCharactersById(id: id, parameters: query ?? .init()))
+	}
+
+	func characters(byKind kind: String, query: CharacterQueryParameters? = nil) async throws -> Ponies {
+		try await client.request(PonyAPI.GetCharactersByKind(kind: kind, parameters: query ?? .init()))
+	}
+
+	func characters(byOccupation occupation: String, query: CharacterQueryParameters? = nil) async throws -> Ponies {
+		try await client.request(PonyAPI.GetCharactersByOccupation(occupation: occupation, parameters: query ?? .init()))
+	}
+
+	func characters(byResidence residence: String, query: CharacterQueryParameters?) async throws -> Ponies {
+		try await client.request(PonyAPI.GetCharactersByResidence(residence: residence, parameters: query ?? .init()))
+	}
+}
+#endif
--- Sources/PonyKit/PonyServiceProtocol.swift
+++ Sources/PonyKit/PonyServiceProtocol.swift
@@ -1,10 +1,22 @@
 import Combine
 import Foundation
 
-protocol PonyServiceProtocol {
+public protocol PonyServiceProtocol {
 	func allCharacters(query: CharacterQueryParameters?) -> AnyPublisher<Ponies, Error>
 	func characters(byId: Int, query: CharacterQueryParameters?) -> AnyPublisher<Ponies, Error>
 	func characters(byKind: String, query: CharacterQueryParameters?) -> AnyPublisher<Ponies, Error>
 	func characters(byOccupation: String, query: CharacterQueryParameters?) -> AnyPublisher<Ponies, Error>
 	func characters(byResidence: String, query: CharacterQueryParameters?) -> AnyPublisher<Ponies, Error>
+#if swift(>=5.5)
+	@available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+	func allCharacters(query: CharacterQueryParameters?) async throws -> Ponies
+	@available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+	func characters(byId: Int, query: CharacterQueryParameters?) async throws -> Ponies
+	@available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+	func characters(byKind: String, query: CharacterQueryParameters?) async throws -> Ponies
+	@available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+	func characters(byOccupation: String, query: CharacterQueryParameters?) async throws -> Ponies
+	@available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+	func characters(byResidence: String, query: CharacterQueryParameters?) async throws -> Ponies
+#endif
 }

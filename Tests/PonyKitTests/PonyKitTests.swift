78ad175408e5c600b470f885d98662e8b2eb824b
Rick <rickb@rickb.com>
2022-03-03 19:48:43 -0600

    update

--- Tests/PonyKitTests/PonyKitTests.swift
+++ Tests/PonyKitTests/PonyKitTests.swift
@@ -10,15 +10,4 @@ final class PonyKitTests: XCTestCase {
 	override class func tearDown() {
 		super.tearDown()
 	}
-
-	func testCharacterEndpoints() {
-		asyncTest {
-			let onePony = try await PonyService.default.allCharacters(query: .init(limit: 1, offset: 1))
-			XCTAssert(onePony.data.count == 1)
-
-			let ponyId1 = try await PonyService.default.characters(byId: 1)
-			XCTAssert(ponyId1.data.count == 1)
-			XCTAssert(ponyId1.data[0].id == 1)
-		}
-	}
 }
